251ac026c788f3dfbd3949e773f6e985816033e7
Sarah Franc <sfranc@student.42.fr>
2019-11-08 11:51:37 +0100

    mount fs script

--- host/mount_lfs.sh
+++ host/mount_lfs.sh
@@ -1,3 +1,6 @@
-PARTITION="$1"
-mkfs -v -t ext4 $1
-mount -v -t ext4 $1 /mnt/lfs
+partition="$1"
+echo "create fs on partition"
+mkfs -v -t ext4 $partition
+echo "mount partition at $LFS"
+mkdir -p $LFS
+mount -v -t ext4 $partition $LFS
